f0d7ca0eb9b56192d8179a19dc58f5a94c8b922e
EliotTaylor1 <104195627+EliotTaylor1@users.noreply.github.com>
2025-02-09 20:59:28 +0000

    chore: move endpoints to /api/

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import morgan from 'morgan'
 import routes from './routes/routes.js'
 
 const app = express()
@@ -9,12 +8,12 @@ const port = process.env.PORT
 
 app.use(express.json())
 app.use(cors());
-app.use(morgan('dev'))
-app.use(routes);
 
 app.get('/', (req, res) => {
   res.send('Success: API is working')
 })
+
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 import {Router} from 'express'
+import morgan from 'morgan'
 import profile from './summoner/profile.controller.js'
 import match from './match/match.controller.js'
 
 const api = Router()
+    .use(morgan('dev'))
     .use(profile)
     .use(match)
 
