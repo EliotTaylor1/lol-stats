125b3f06446bfd41f98bfd81aa938d3959dea7c4
EliotTaylor1 <104195627+EliotTaylor1@users.noreply.github.com>
2025-02-26 22:13:57 +0000

    feat: display item and champion images

--- frontend/src/Match/MatchParticipants.jsx
+++ frontend/src/Match/MatchParticipants.jsx
@@ -1,26 +1,67 @@
 import './MatchParticipant.css'
 
+function getItemImageString(itemId) {
+    return `https://ddragon.leagueoflegends.com/cdn/15.4.1/img/item/${itemId}.png`
+} 
+
+function getChampionPortrait(championName) {
+    return `https://ddragon.leagueoflegends.com/cdn/15.4.1/img/champion/${championName}.png`
+}
+
+function getSummonerSpellImageFromId(summonerSpellId) {
+    switch (summonerSpellId) {
+        case 1:
+            return "https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerCleanse.png"
+        case 3:
+            return "https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerExhaust.png"
+        case 4:
+            return "https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerFlash.png"
+        case 6:
+            return "https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerHaste.png"
+        case 7:
+            return "https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerHeal.png"
+        case 11:
+            return "https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerSmite.png"
+        case 12:
+            return "https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerTeleport.png"
+        case 13:
+            return "https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerMana.png"
+        case 14:
+            return "https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerDot.png"
+        case 21:
+            return "https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerBarrier.png"
+        case 32:
+            return "https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerSnowball.png"
+    }
+}
+
 export function MatchParticipant({ participant }) {
+    // the build object contains more than just the 6 items a player builds
+    // this array makes it easier to iterate over just the 6 items we want to display
+    const itemKeys = ["item_0", "item_1", "item_2", "item_3", "item_4", "item_5"];
+
     return (
         <div className="participant-card">
             <p>{participant.team_position}</p>
             <p>{participant.summoner.summoner_name} #{participant.summoner.summoner_tag}</p>
-            <p>{participant.champion_id}</p>
-            <p>{participant.performance.champion_level}</p>
+            <img className='champion' src={getChampionPortrait(participant.champion_name)} alt={participant.champion_name}></img>
+            <p>Level: {participant.performance.champion_level}</p>
             <p>K/D/A: {participant.performance.kills}/{participant.performance.deaths}/{participant.performance.assists}</p>
             <p>Largest Multi-kill: {participant.performance.largest_multi_kill}</p>
-            <p>Gold Earned: {participant.performance.gold_earned}</p>
-            <p>{participant.build.item_0}</p>
-            <p>{participant.build.item_1}</p>
-            <p>{participant.build.item_2}</p>
-            <p>{participant.build.item_3}</p>
-            <p>{participant.build.item_4}</p>
-            <p>{participant.build.item_5}</p>
-            <p>{participant.build.item_6}</p>
-            <p>{participant.build.summoner_1_id}</p>
-            <p>{participant.build.summoner_2_id}</p>
-            <p>{participant.build.stealth_wards_bought}</p>
-            <p>{participant.build.vision_wards_bought}</p>
+            <p>Gold: {participant.performance.gold_earned}</p>
+
+            <div className='build-icons-container'>
+                {itemKeys.map((key, index) => 
+                    participant.build[key] !== 0 && <img key={index} src={getItemImageString(participant.build[key])} />
+                )}
+            </div>
+
+            <div className='summoner-spell-trinket-container'>
+                <img className='trinket' src={getItemImageString(participant.build.item_6)}></img>
+                <img className='summoner-spell' src={getSummonerSpellImageFromId(participant.build.summoner_1_id)}></img>
+                <img className='summoner-spell' src={getSummonerSpellImageFromId(participant.build.summoner_2_id)}></img>
+            </div>
+            <p>Vision Wards:{participant.build.vision_wards_bought}</p>
         </div>
     )
 }
